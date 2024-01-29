080b66f79fd592c75f7f6a3f5681e8a541dc1e37
Marcos Mantovani <marcospiderplays@gmail.com>
2024-01-29 00:34:36 -0300

    Convertendo a estilização com styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
+import { ThemeProvider } from 'styled-components'
+
 import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
-
-import './global.css'
+import EstiloGlobal, { Container } from './styles'
+import defaultTheme from './Themes/default'
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
+      <EstiloGlobal />
       <Header />
       <Hero />
-      <div className="container">
+      <Container>
         <ListaVagas />
-      </div>
-    </>
+      </Container>
+    </ThemeProvider>
   )
 }
 
---
+++ src/Themes/default.ts
@@ -0,0 +1,14 @@
+const defaultTheme = {
+  corPrincipal: '#a7727d',
+  corSecundaria: '#f9f5e7'
+}
+
+export default defaultTheme
+
+export type Theme = {
+  corPrincipal: string
+  corSecundaria: string
+  corDeFundo: string
+  corDeFundoBotao: string
+  corDaBorda: string
+}
